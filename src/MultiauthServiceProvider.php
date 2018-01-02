ab45306c58f521b6f4c3e7af29c64ce4437ca51d
Danilo <danilo.tapiah@gmail.com>
2018-01-02 11:28:00 -0300

    first commit

---
+++ src/MultiauthServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Jumpitt\Passport_multiauth;
+
+use Illuminate\Support\ServiceProvider;
+
+class MultiauthServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/middleware' => base_path('app/Http/Middleware'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/editauth.php', 'auth.guards'
+        );
+    }
+}
---
+++ src/middleware/CheckGuard.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Jumpitt\Passport_multiauth;
+
+use Auth;
+use Closure;
+use App\Customer;
+use App\User;
+use App\Admin;
+use Lcobucci\JWT\Parser;
+use Laravel\Passport\TokenRepository;
+
+class CheckGuard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard)
+    {
+        $headers = apache_request_headers();
+        $bearer = $headers['Authorization'];
+
+        $bearerT = substr($bearer, 7);
+        
+        $jwt = (new Parser())->parse($bearerT);
+        $tokenRepository = new TokenRepository();
+        $token= $tokenRepository->find($jwt->getClaim('jti'));
+
+        switch ($guard) {
+            case 'admin':
+                $client_id = ADMIN::PASSPORT;
+                break;
+            case 'customer':
+                $client_id = CUSTOMER::PASSPORT;
+                break;
+            default:
+                $client_id = USER::PASSPORT;
+                break;
+        }
+
+        if ($token->client_id == $client_id) {
+            return $next($request);
+        } else {
+            dd("no entras");
+        }
+
+    }
+}
